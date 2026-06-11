c97cdfa5c08f47ba8936eb2dba9952ba3348d448
Alexandre Gabriel de Souza Santos <alegss25@gmail.com>
2026-06-11 20:13:34 -0300

    Configurada url da api

--- App.js
+++ App.js
@@ -8,20 +8,23 @@ export default function App() {
   const [quantidade, setQuantidade] = useState('');
   const [materials, setMaterials] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const API_URL = "https://6a2b3ed1b687a7d5cbc50251.mockapi.io/api/v1/materials";
+
   // --- Funções de Requisição e Efeitos (Os alunos implementarão aqui) ---
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Almoxarifado - Enfermagem</Text>
-      
+
       {/* Breve descrição do projeto inserida abaixo */}
       <Text style={styles.description}>
-        Este template servirá para desenvolver o projeto responsável por modernizar o controle de insumos médicos do almoxarifado. 
+        Este template servirá para desenvolver o projeto responsável por modernizar o controle de insumos médicos do almoxarifado.
         Através desta interface conectada à API, é possível realizar o inventário em tempo real, cadastrar novos materiais e registrar baixas de estoque de forma ágil e segura.
       </Text>
 
       {/* Os alunos vão construir os componentes visuais das Sprints aqui dentro */}
-      
+
     </View>
   );
 }
@@ -37,14 +40,14 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 10, // Reduzido ligeiramente para aproximar o texto explicativo
+    marginBottom: 10,
     color: '#333',
   },
   description: {
     fontSize: 14,
     color: '#666',
     textAlign: 'center',
-    lineHeight: 20, // Dá um espaçamento confortável entre as linhas do parágrafo
-    marginBottom: 30, // Margem inferior para afastar o texto dos futuros inputs dos alunos
+    lineHeight: 20,
+    marginBottom: 30,
   }
 });
